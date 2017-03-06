d9e72d55f35f1f6d1c18a80ff244265557288d20
Keir Sweeney <keir.sweeney@cohaesus.co.uk>
2017-03-06 14:22:27 +0000

    Build project

--- src/index.js
+++ src/index.js
@@ -8,12 +8,8 @@ const defaultState = {
   appName: 'conduit',
   articles: null
  };
- 
+
 const reducer = function(state = defaultState, action) {
-  switch (action.type) {
-    case 'TOGGLE':
-      return { ...state, checked: !state.checked };
-  }
   return state;
 }
 
